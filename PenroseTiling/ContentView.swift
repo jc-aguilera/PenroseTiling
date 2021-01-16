ee8188a4d95688e1ff88d0c2a8281bcb6f842068
Juan Carlos Aguilera <jcaguile@uc.cl>
2021-01-16 16:21:29 -0300

    Initial Commit

---
+++ PenroseTiling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PenroseTiling
+//
+//  Created by Juan Carlos Aguilera Núñez on 16-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PenroseTiling/PenroseTilingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenroseTilingApp.swift
+//  PenroseTiling
+//
+//  Created by Juan Carlos Aguilera Núñez on 16-01-21.
+//
+
+import SwiftUI
+
+@main
+struct PenroseTilingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
